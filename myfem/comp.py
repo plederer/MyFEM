4ff038e3e2a755994b92b5f630f9d6495bf6daa1
Philip <philip@u24.at>
2024-01-05 15:07:06 +0100

    working

--- myfem/comp.py
+++ myfem/comp.py
@@ -36,13 +36,38 @@ class Laplace():
         self.coeff = coeff
 
     def Assemble(self):
-        print("in Assemble")
-        # for e in self.space.mesh.els
-        #...
-        
+        # print("in Assemble")
+        for nr, el in enumerate(self.space.mesh.els):
+            lndofs = self.space.fem.ndofs
+            A_T = np.zeros((lndofs,lndofs))
+
+            ir = IntegrationRule()
+
+            trafo = MeshTrafo(nr, self.space.mesh)
+
+            for ip in ir.points:
+                omega = ip[1]
+                x = ip[0][0]
+                y = ip[0][1]
+                
+                Dphi_hat = self.space.fem.DEvaluate(x,y) @ trafo.Dinv
+                A_T += trafo.J * omega * self.coeff * (Dphi_hat @ Dphi_hat.T) 
+
+            for i in range(lndofs):
+                for j in range(lndofs):
+                    self.mat[el[i],el[j]] += A_T[i,j]
+            
     def CalcInverse(self):
-        print("in CalcInverse")
-        #...
+        dofs = []
+        for i in range(self.space.ndof):
+            if self.space.FreeDofs[i]:
+                dofs.append(i)
+        # print(dofs)
+        submat = self.mat[np.ix_(dofs,dofs)]
+        # print(submat)
+        submat_inv = np.linalg.inv(submat)
+        self.invmat[np.ix_(dofs,dofs)] = submat_inv
+
 
 class Source():
     def __init__(self, space, load = 1):
@@ -51,5 +76,19 @@ class Source():
         self.load = load
 
     def Assemble(self):
-        print("in Assemble")
-        #...
+        for nr, el in enumerate(self.space.mesh.els):
+            lndofs = self.space.fem.ndofs
+            F_T = np.zeros(lndofs)
+
+            ir = IntegrationRule()
+            trafo = MeshTrafo(nr, self.space.mesh)
+
+            for ip in ir.points:
+                omega = ip[1]
+                xhat = ip[0][0]
+                yhat = ip[0][1]
+                x,y = trafo.map(xhat,yhat)
+                F_T += trafo.J * omega * self.load(x,y) * self.space.fem.Evaluate(xhat,yhat)
+                
+            for i in range(lndofs):
+                    self.vec[el[i]] += F_T[i]
--- myfem/mesh.py
+++ myfem/mesh.py
@@ -1,24 +1,39 @@
-
-
+import numpy as np 
     
-class IntegrationRule():
-    def ___init__(self):
-        self.points = np.array([1/3,1/3])
-        self.weights = np.array([1])
-        self.order = 1
+class IntegrationRule:
+    def __init__(self, order = 1):
+        # (x,y),omega
+        # self.points = [[(1/3,1/3),1]]
+        self.points = [[(1,0),1/3],
+                       [(0,1),1/3],
+                       [(0,0),1/3]]
+        self.order = order
 
-class Mesh():
+class Mesh:
     def __init__(self, ngsmesh):
         self.ngsmesh = ngsmesh
         self.nv = ngsmesh.nv
-        self.pnts = [p for p in ngsmesh.vertices]
+        self.pnts = [p.point for p in ngsmesh.vertices]
         self.els = [[v.nr for v in e.vertices] for e in ngsmesh.Elements()]
 
     def GetSurfaceElements(self, bnd):
         return [[v.nr for v in e.vertices] for e in self.ngsmesh.Boundaries(bnd).Elements()]
     
 class MeshTrafo:
-    def __init__(self):
-        self.mesh = None
-        print("initi of Trafo")
+    def __init__(self, nr, mesh):
+        self.ev = mesh.els[nr]
+        
+        V0 = mesh.pnts[self.ev[0]]
+        V1 = mesh.pnts[self.ev[1]]
+        V2 = mesh.pnts[self.ev[2]]
+        
+        # phi = D * x + b
+        self.D = np.array([[V1[0]-V0[0],V2[0]-V0[0]],[V1[1]-V0[1],V2[1]-V0[1]]])
+        self.b = np.array(V0)
+        self.J = np.abs(self.D[0,0] * self.D[1,1] - self.D[1,0] * self.D[0,1])
+        self.Dinv = 1/self.J * np.array([[self.D[1,1],-self.D[0,1]], [-self.D[1,0], self.D[0,0]]])
     
+    def map(self, xhat, yhat):
+        Xhat = np.array([xhat,yhat])
+        return self.D @ Xhat + self.b
+
